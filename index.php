9a4fe9d16654c175e6ae8110fa9cf760d7fd41bd
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-05-16 04:01:10 +0200

    add-index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop Online</title>
+</head>
+<body>
+    <h1>Welcome to Animal Shop</h1>
+
+    <?php
+    require_once 'classes.php';
+
+
+
+</body>
+</html>
