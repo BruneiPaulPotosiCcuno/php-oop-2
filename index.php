b5c1c0e1325e95af66ac5e576881e55463b93a01
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-05-21 19:45:53 +0200

    add-models

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
+<?php
+
+// Includi il file 'AnimalProducts.php' che si trova nella directory 'models'
+require_once __DIR__ . '/models/AnimalProducts.php';
+// Includi il file 'CatsProducts.php' che si trova nella directory 'models'
+require_once __DIR__ . '/models/CatsProducts.php';
+// Includi il file 'DogsProducts.php' che si trova nella directory 'models'
+require_once __DIR__ . '/models/DogsProducts.php';
+
+?>
+
+<!-- //template -->
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop Online</title>
+    <title></title>
 </head>
 <body>
-    <h1>Welcome to Animal Shop</h1>
-
-    <?php
-    require_once 'classes.php';
-
-
 
 </body>
 </html>
