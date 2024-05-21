68a3ad89a125db7ea5168da188fe7a08df67dc0b
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-05-21 23:05:26 +0200

    fix-requires

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <?php
 
-// Includi il file 'AnimalProducts.php' che si trova nella directory 'models'
-require_once __DIR__ . '/models/AnimalProducts.php';
-// Includi il file 'CatsProducts.php' che si trova nella directory 'models'
-require_once __DIR__ . '/models/CatsProducts.php';
-// Includi il file 'DogsProducts.php' che si trova nella directory 'models'
-require_once __DIR__ . '/models/DogsProducts.php';
+
+require_once __DIR__ . '/Models/Product.php';
+require_once __DIR__ . '/Models/Food.php';
+require_once __DIR__ . '/Models/Game.php';
+require_once __DIR__ . '/Models/Kennel.php';
+require_once __DIR__ . '/Models/Category.php';
+
+$dogs = new Category('Cani', 'https://picsum.photos/200/300');
+$cats = new Category('Felini', 'https://picsum.photos/200/300');
+
+$collare = new Product('Collare in pelle', 19.99, $dogs);
+
+
+$crocchette = new Food('Crocchette Premium', 24.50, $dogs, ['pollo', 'riso']);
+$crocchette->description = 'Le crocchette più nutrienti e deliziose';
+
+
+$pallina = new Game('Pallina Interattiva', 9.99, $cats);
+
+
+$luxuryImperialKennel = new Kennel('Cuccia Regale', 899.00, $cats);
+$luxuryImperialKennel->size = 8;
+
+$products = [
+    $collare,
+    $crocchette,
+    $pallina,
+    $luxuryImperialKennel
+];
 
 ?>
+
 
 <!-- //template -->
 
