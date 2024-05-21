8e785fa1da01c2cba849955772a80515c30abe78
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-05-21 23:11:02 +0200

    add-requires

--- Models/Product.php
+++ Models/Product.php
@@ -1,3 +1,16 @@
 <?php
+class Product {
 
+    public $name;
+    public $description;
+    public $price;
+    public $image;
+    public $category;
+
+    public function __construct($_name, $_price, Category $_category) {
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->category = $_category;
+    }
+}
 ?>
